5f0af0fb3b880e1d1bb961ed5726e316e727cc2f
BrokenShell <webmaster@sharpdesigndigital.com>
2021-06-09 09:32:08 -0700

    cleans up db ops

--- app/db_ops.py
+++ app/db_ops.py
@@ -10,72 +10,52 @@ db_url = os.getenv('DB_URL')
 table_name = "training"
 
 
-def initialize_db():
-    query = f"""CREATE TABLE IF NOT EXISTS {table_name} (
-        id SERIAL PRIMARY KEY NOT NULL,
-        tweets TEXT NOT NULL,
-        labels INT NOT NULL);"""
+def db_action(sql_action: str):
     conn = psycopg2.connect(db_url)
     curs = conn.cursor()
-    curs.execute(query)
+    curs.execute(sql_action)
     conn.commit()
     curs.close()
     conn.close()
 
 
-def insert_data(tweet: str, label: int):
+def db_query(sql_query) -> list:
     conn = psycopg2.connect(db_url)
     curs = conn.cursor()
-    query = f"""
-    INSERT INTO {table_name} 
-    (tweets, labels)
-    VALUES ('{tweet}',{label});"""
-    curs.execute(query)
-    conn.commit()
-    curs.close()
-    conn.close()
-
-
-def load_data() -> list:
-    conn = psycopg2.connect(db_url)
-    curs = conn.cursor()
-    query = f"SELECT * FROM {table_name};"
-    curs.execute(query)
+    curs.execute(sql_query)
     results = curs.fetchall()
     curs.close()
     conn.close()
     return results
 
 
+def initialize_db():
+    db_action(f"""CREATE TABLE IF NOT EXISTS {table_name} (
+    id SERIAL PRIMARY KEY NOT NULL,
+    tweets TEXT NOT NULL,
+    labels INT NOT NULL);""")
+
+
+def insert_data(tweet: str, label: int):
+    db_action(f"""INSERT INTO {table_name} 
+    (tweets, labels) 
+    VALUES ('{tweet}',{label});""")
+
+
+def load_data() -> list:
+    return db_query(f"SELECT * FROM {table_name};")
+
+
+def load_by_id(idx: int) -> list:
+    return db_query(f"SELECT * FROM {table_name} WHERE id = {idx};")
+
+
 def reset_table():
-    conn = psycopg2.connect(db_url)
-    curs = conn.cursor()
-    query = f"TRUNCATE TABLE {table_name} RESTART IDENTITY;"
-    curs.execute(query)
-    conn.commit()
-    curs.close()
-    conn.close()
+    db_action(f"TRUNCATE TABLE {table_name} RESTART IDENTITY;")
 
 
-def get_last_id() -> int:
-    conn = psycopg2.connect(db_url)
-    curs = conn.cursor()
-    query = f"SELECT MAX(id) FROM {table_name};"
-    curs.execute(query)
-    results = curs.fetchone()[0]
-    curs.close()
-    conn.close()
-    return results or 0
-
-
-def delete_by_id(idx):
-    conn = psycopg2.connect(db_url)
-    curs = conn.cursor()
-    query = f"DELETE FROM {table_name} WHERE id = {idx};"
-    curs.execute(query)
-    conn.commit()
-    curs.close()
-    conn.close()
+def delete_by_id(idx: int):
+    db_action(f"DELETE FROM {table_name} WHERE id = {idx};")
 
 
 if __name__ == '__main__':
